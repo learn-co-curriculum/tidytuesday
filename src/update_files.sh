e671930ae6c7123210e0c58dc0455ddac3b139e8
Joél Collins <joelsewhere@gmail.com>
2022-09-09 13:17:31 -0500

    fixing path issue

--- src/update_files.sh
+++ src/update_files.sh
@@ -2,6 +2,8 @@ git checkout master
 git pull origin master
 git checkout flatiron
 git checkout master data/
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "${parent_path}"
 python scrub_readme.py
 git add data
 git commit -m 'updating data files'
